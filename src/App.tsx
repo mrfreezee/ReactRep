1e813e8d189fbb6b830a0b2b946b3d245a4742b0
mrfreezee <mrfrezee27@mail.ru>
2023-11-29 03:29:26 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import './App.css';
 import { SignUp } from './components/Title/Title'
 import { Header } from './components/Header/Header'
 import {Tabs} from './components/Tabs/Tabs';
-import { BigCard } from './components/Cards/BigCard/BigCard';
-import { MediumCard } from './components/Cards/MediumCard/MediumCard';
-import { SmallCard } from './components/Cards/SmallCard/SmallCard'
-import { PostsList } from './components/Cards/PostsList';
+import { PostsList } from './components/PostsList/PostsList';
 
 
 
@@ -16,14 +13,8 @@ function App() {
   return (
     <>
     <Header/>
-    {/* <Tabs/> */}
+    <Tabs/>
     <PostsList/>
-    {/* <SignUp name={'Sign In'}/>
-    
-    <BigCard/>
-    <MediumCard/>
-    <SmallCard/> */}
-    {/* <TabsWrapper/> */}
     </>
   );
 }
--- src/components/Header/HeaderComponents/Search/Search.tsx
+++ src/components/Header/HeaderComponents/Search/Search.tsx
@@ -2,29 +2,11 @@ import {useState, ChangeEvent} from 'react'
 import styles from './Search.module.scss'
 import {SearchIcon} from './SeacrhIcon'
 
-type Props = {
-    open?: boolean
-    onClickOnIcon: () => void
-    event?: () => void
-}
 
-// export const SearchInput = ({open}: Props) =>{
-
-//     const isOpenStyle = open ? styles.open : ''
-
-//     return(
-        
-//     )
-// }
-type InputWithToggleProps = {
-    openInput: boolean
-    handleToogle: () => void
-}
 
 export const Search = () => {
 
     const [openInput, setOpenInput] = useState<boolean>(false)
-    // const [inputValue, setInputValue] = useState<string>('');
 
     const handleToogle = () =>{
         setOpenInput(!openInput)
@@ -37,7 +19,6 @@ export const Search = () => {
             <input type='text' className={styles.searchInput} placeholder='Search...'/> }
             <div className={styles.wrapperSearchIcon} onClick={handleToogle}>
             {openInput ? <SearchIcon/> : <SearchIcon/>}
-                {/* <img className={styles.searchIcon} src={icon} onClick={handleToogle}>{openInput ? 'X' : 'O'}</img> */}
             </div>
         </div>
     )
